2c256cdd78c0d041c9cc4b90ef484a9ddc3d8fdc
lukmanis25 <lukassmolinski@wp.pl>
2024-05-27 22:14:47 +0200

    add base gtk

--- app_gtk3.py
+++ app_gtk3.py
@@ -4,6 +4,7 @@ import json
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, Pango
+
 from board import Board
 
 class SudokuSolver(Gtk.Window):
@@ -16,14 +17,29 @@ class SudokuSolver(Gtk.Window):
         self.update_board_view()
 
     def init_ui(self):
+        vbox = Gtk.VBox()
+        self.add(vbox)
+
+        self.create_menu(vbox)
+
+        style_provider = Gtk.CssProvider()
+        style_provider.load_from_data(b"""
+            .square-button {
+                border-radius: 0;
+                border: 5px solid black;
+            }
+        """)
+
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        vbox.pack_start(hbox, True, True, 0)
+
+        self.create_board_layout(hbox)
+        self.create_panel_layout(hbox)
+
+    def create_board_layout(self, hbox):
         grid = Gtk.Grid()
-        self.add(grid)
-
-        self.create_board_layout(grid)
-        self.create_panel_layout(grid)
-        self.create_menu()
-
-    def create_board_layout(self, grid):
+        hbox.pack_start(grid, True, True, 0)
+        
         self.cell_views = []
         for i in range(9):
             for j in range(9):
@@ -32,14 +48,16 @@ class SudokuSolver(Gtk.Window):
                 self.cell_views.append(cell)
 
     def create_board_cell(self, i, j):
-        cell = Gtk.Button(label="")
+        cell = Gtk.Button(label="", name="square-button")
         cell.modify_font(Pango.FontDescription("Arial 20"))
-        cell.modify_bg(Gtk.StateType.NORMAL, Gdk.Color.parse("white")[1])
+        cell.modify_bg(Gtk.StateType.NORMAL, Gdk.color_parse("white"))
         cell.connect("clicked", self.select_cell_event, i, j)
         return cell
 
-    def create_panel_layout(self, grid):
+    def create_panel_layout(self, hbox):
         panel_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        hbox.pack_start(panel_box, False, False, 0)
+
         for number in range(1, 10):
             button = Gtk.Button(label=str(number))
             button.set_size_request(50, 50)
@@ -57,31 +75,30 @@ class SudokuSolver(Gtk.Window):
         new_board_button.connect("clicked", self.generate_board)
         panel_box.add(new_board_button)
 
-        grid.attach(panel_box, 9, 0, 1, 9)
-
-    def create_menu(self):
+    def create_menu(self, vbox):
         menu_bar = Gtk.MenuBar()
+        vbox.pack_start(menu_bar, False, False, 0)
 
         file_menu = Gtk.Menu()
-        save_action = Gtk.MenuItem("Zapisz grę")
+        save_action = Gtk.MenuItem(label="Zapisz grę")
         save_action.connect("activate", self.save_game_event)
-        file_menu.append(save_action)
-        load_action = Gtk.MenuItem("Wczytaj grę")
+        load_action = Gtk.MenuItem(label="Wczytaj grę")
         load_action.connect("activate", self.load_game)
+        file_menu.append(save_action)
         file_menu.append(load_action)
+
         file_menu_item = Gtk.MenuItem(label="Plik")
         file_menu_item.set_submenu(file_menu)
         menu_bar.append(file_menu_item)
 
         options_menu = Gtk.Menu()
-        about_action = Gtk.MenuItem("O aplikacji")
+        about_action = Gtk.MenuItem(label="O aplikacji")
         about_action.connect("activate", self.show_about_event)
         options_menu.append(about_action)
+
         options_menu_item = Gtk.MenuItem(label="Opcje")
         options_menu_item.set_submenu(options_menu)
         menu_bar.append(options_menu_item)
-
-        self.add(menu_bar)
 
     def update_board_view(self):
         for i in range(9):
@@ -98,7 +115,19 @@ class SudokuSolver(Gtk.Window):
                     cell_style += "color: red;"
 
                 cell_view.get_style_context().add_class("button")
-                cell_view.get_style_context().set_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1, 1, 1, 1))
+                #cell_view.get_style_context().set_background_color(Gtk.StateType.NORMAL, Gdk.RGBA(1, 1, 1, 1))
+
+    def get_default_cell_style(self, i, j):
+        style = "background-color: white; border: 1px solid black;"
+        if i % 3 == 0 and i != 0:
+            style += "border-top: 2px solid black;"
+        if j % 3 == 0 and j != 0:
+            style += "border-left: 2px solid black;"
+        if (i + 1) % 3 == 0 and (i + 1) != 9:
+            style += "border-bottom: 2px solid black;"
+        if (j + 1) % 3 == 0 and (j + 1) != 9:
+            style += "border-right: 2px solid black;"
+        return style
 
     def select_cell_event(self, widget, i, j):
         self.board.set_selected_cell(i,j)
@@ -117,7 +146,8 @@ class SudokuSolver(Gtk.Window):
         self.update_board_view()
 
     def save_game_event(self, widget):
-        dialog = Gtk.FileChooserDialog("Zapisz grę", self, Gtk.FileChooserAction.SAVE, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.ACCEPT))
+        dialog = Gtk.FileChooserDialog("Zapisz grę", self, Gtk.FileChooserAction.SAVE, 
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_SAVE, Gtk.ResponseType.ACCEPT))
         dialog.set_do_overwrite_confirmation(True)
         response = dialog.run()
         if response == Gtk.ResponseType.ACCEPT:
@@ -126,7 +156,8 @@ class SudokuSolver(Gtk.Window):
         dialog.destroy()
 
     def load_game(self, widget):
-        dialog = Gtk.FileChooserDialog("Wczytaj grę", self, Gtk.FileChooserAction.OPEN, (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT))
+        dialog = Gtk.FileChooserDialog("Wczytaj grę", self, Gtk.FileChooserAction.OPEN, 
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, Gtk.STOCK_OPEN, Gtk.ResponseType.ACCEPT))
         response = dialog.run()
         if response == Gtk.ResponseType.ACCEPT:
             file_path = dialog.get_filename()
@@ -135,7 +166,8 @@ class SudokuSolver(Gtk.Window):
         dialog.destroy()
 
     def show_about_event(self, widget):
-        dialog = Gtk.MessageDialog(parent=self, flags=0, message_type=Gtk.MessageType.INFO, buttons=Gtk.ButtonsType.OK, text="SudokuSolver to aplikacja do rozwiązywania sudoku.\n\nAutor: Łukasz Smoliński 184306")
+        dialog = Gtk.MessageDialog(parent=self, flags=0, message_type=Gtk.MessageType.INFO, 
+                                   buttons=Gtk.ButtonsType.OK, text="SudokuSolver to aplikacja do rozwiązywania sudoku.\n\nAutor: Łukasz Smoliński 184306")
         dialog.run()
         dialog.destroy()
 
